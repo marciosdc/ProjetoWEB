df29d0f4ccfd6ef5eae57d6b10a1945668c373ae
marciosdc <marciosdc@gmail.com>
2015-03-06 10:01:43 -0300

    Commit

--- Academico/Academico/JavaSource/Commit.java
+++ Academico/Academico/JavaSource/Commit.java
@@ -1,14 +1,9 @@
 
 public class Commit
 {
-<<<<<<< Upstream, based on origin/master
-=======
 	private String teste2;
 	private String ola;
-<<<<<<< Upstream, based on origin/master
->>>>>>> 3518f46 Corrigindo conflitos
+
 	private String teste1;
 	private String nome;
-=======
->>>>>>> e58a91f Rebase
 }
