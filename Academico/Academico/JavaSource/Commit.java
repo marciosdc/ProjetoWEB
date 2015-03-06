1ce5bd74e314a666721db69b8e8070dd090605fb
marciosdc <marciosdc@gmail.com>
2015-03-06 09:57:55 -0300

    Rebase

--- Academico/Academico/JavaSource/Commit.java
+++ Academico/Academico/JavaSource/Commit.java
@@ -5,7 +5,10 @@ public class Commit
 =======
 	private String teste2;
 	private String ola;
+<<<<<<< Upstream, based on origin/master
 >>>>>>> 3518f46 Corrigindo conflitos
 	private String teste1;
 	private String nome;
+=======
+>>>>>>> e58a91f Rebase
 }
