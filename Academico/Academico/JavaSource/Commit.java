9f0c0b1f2929126632a685367ef145b72cbfd577
marciosdc <marciosdc@gmail.com>
2015-03-06 09:47:22 -0300

    Corrigindo conflitos

--- Academico/Academico/JavaSource/Commit.java
+++ Academico/Academico/JavaSource/Commit.java
@@ -1,6 +1,11 @@
 
 public class Commit
 {
+<<<<<<< Upstream, based on origin/master
+=======
+	private String teste2;
+	private String ola;
+>>>>>>> 3518f46 Corrigindo conflitos
 	private String teste1;
 	private String nome;
 }
